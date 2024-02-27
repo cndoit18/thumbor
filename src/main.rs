ad8eb8e93a7c90cbde6ec55b0df15ca207bbe9f8
cndoit18 <cndoit18@outlook.com>
2024-02-28 02:50:36 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,100 @@
+use anyhow::Result;
+use axum::{
+    extract::{Extension, Path},
+    http::{HeaderMap, HeaderValue, StatusCode},
+    routing::get,
+    Router,
+};
+use bytes::Bytes;
+use lru::LruCache;
+use percent_encoding::percent_decode_str;
+use serde::Deserialize;
+use std::{
+    collections::hash_map::DefaultHasher,
+    convert::TryInto,
+    hash::{Hash, Hasher},
+    num::NonZeroUsize,
+    sync::Arc,
+};
+use tokio::sync::Mutex;
+use tower::ServiceBuilder;
+use tracing::{info, instrument};
+
+mod pb;
+use pb::*;
+
+mod engine;
+use engine::{Engine, Photon};
+use image::ImageOutputFormat;
+
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+type Cache = Arc<Mutex<LruCache<u64, Bytes>>>;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let cache: Cache = Arc::new(Mutex::new(LruCache::new(NonZeroUsize::new(1024).unwrap())));
+
+    let app = Router::new()
+        .route("/image/:spec/:url", get(generate))
+        .layer(ServiceBuilder::new().layer(Extension(cache)).into_inner());
+
+    let addr = "127.0.0.1:3000";
+    tracing::debug!("listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn generate(
+    Path(Params { spec, url }): Path<Params>,
+    Extension(cache): Extension<Cache>,
+) -> Result<(HeaderMap, Vec<u8>), StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    let data = retrieve_image(&url, cache)
+        .await
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    let mut engine: Photon = data
+        .try_into()
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    engine.apply(&spec.specs);
+    let image = engine.generate(ImageOutputFormat::Jpeg(85));
+    info!("Finished processing: image size {}", image.len());
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", HeaderValue::from_static("image/jpeg"));
+    Ok((headers, image))
+}
+
+#[instrument(level = "info", skip(cache))]
+async fn retrieve_image(url: &str, cache: Cache) -> Result<Bytes> {
+    let mut hasher = DefaultHasher::new();
+    url.hash(&mut hasher);
+    let key = hasher.finish();
+    let g = &mut cache.lock().await;
+    let data = match g.get(&key) {
+        Some(v) => {
+            info!("Match cache {}", key);
+            v.to_owned()
+        }
+        None => {
+            info!("Retrieve url");
+            let resp = reqwest::get(url).await?;
+            let data = resp.bytes().await?;
+            g.put(key, data.clone());
+            data
+        }
+    };
+    Ok(data)
+}
